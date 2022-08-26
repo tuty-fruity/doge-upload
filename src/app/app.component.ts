864c155ef102f002e2a1e6e6ce7463ce1c106d5a
John Rey Tingson <john.tingson@coexservices.com.au>
2022-08-26 21:41:56 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'doge-upload';
+
+  private imgSrc: string;
+
+  constructor() {
+      this.imgSrc = "";
+  }
+
+  onDragOver(e: Event) : void {
+    e.preventDefault();
+  }
+
+  onDragLeave(e: Event) : void {
+    e.preventDefault()
+  }
+
+  onDrop(e: DragEvent) : void {
+    e.preventDefault();
+
+    if (e.dataTransfer !== null) { 
+        const targetFile = e.dataTransfer.files[0]
+        const reader = new FileReader();
+        reader.readAsDataURL(targetFile);
+        reader.onloadend = (e) => {
+            this.imgSrc = reader.result as string;
+        }
+    }
+  }
+
+  public getImgSrc() : string {
+    return this.imgSrc;
+  }
 }
